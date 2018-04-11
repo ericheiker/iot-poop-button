6cfa251be788d888a122d0357b7b53a1476e4492
eheiker <eheiker@twilio.com>
2018-04-10 17:28:48 -0700

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const twilio = require('twilio')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 const accountSid = process.env.TWILIO_ACCOUNT
 const authToken = process.env.TWILIO_TOKEN
